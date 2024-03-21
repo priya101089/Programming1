4313a4b6806e04728583b32a6702978625b57da0
priya101089 <161402446+priya101089@users.noreply.github.com>
2024-03-20 23:58:13 -0400

    Adding Validator.java source file

---
+++ Validator.java
@@ -0,0 +1,14 @@
+
+public class Validator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	//creating a dummy method
+	public static boolean isAlphaNum(char charToValidate) {
+		boolean isAlphaNumReturn= true;
+		return isAlphaNumReturn;
+	}
+}
