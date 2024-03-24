6fe2d1e2c49de4b1cd29680ac9e5de1667b6fc53
kirillKnyaz <161050906+kirillKnyaz@users.noreply.github.com>
2024-03-24 16:58:29 -0400

    Update Validator.java

--- Validator.java
+++ Validator.java
@@ -3,12 +3,37 @@ public class Validator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello world")
 	}
 
 	//creating a dummy method
-	public static boolean isAlphaNum(char charToValidate) {
-		boolean isAlphaNumReturn= true;
-		return isAlphaNumReturn;
+	public static boolean isAlphaNum(char inputChar) {
+		boolean isAlphaNum = ( Character.isLetter(inputChar) || Character.isDigit(inputChar));
+		return isAlphaNum;
+	}
+	
+	public static boolean isSpecialChar(char inputChar, boolean argument) {
+		char[] acceptableValues = {'-','.'};
+		boolean isSpecialChar = false;
+		
+		for (char n: acceptableValues) {
+			if (n == inputChar) isSpecialChar = true;
+		}
+		
+		if (argument) {
+			if (inputChar == '_') isSpecialChar = true;
+		}
+		
+		return isSpecialChar;
+	}
+	
+	public static boolean isPrefixChar(char inputChar) {
+		boolean isPrefixChar = (isSpecialChar(inputChar,true)||isAlphaNum(inputChar));
+		return isPrefixChar;
+	}
+	
+	public static boolean isDomainChar(char inputChar) {
+		boolean isDomainChar = (isSpecialChar(inputChar,false)||isAlphaNum(inputChar));
+		return isDomainChar;
 	}
 }
